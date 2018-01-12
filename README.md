db8251f957c0055737e5c0cdc652c56be3a84035
Sun Xiaofei <adoni1203@gmail.com>
2018-01-11 21:09:28 -0500

    Fix #2; Update Readme

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ This is the implementation of word2vec based on PyTorch.
 
 Run it with
 
-    python word2vec.py ./zhihu.txt word_embedding.txt
+    python word2vec.py zhihu.txt word_embedding.txt
+
+
+I write a [blog](https://adoni.github.io/2017/11/08/word2vec-pytorch/) about the word2vec based on PyTorch.
--- model.py
+++ model.py
@@ -67,7 +67,6 @@ class SkipGramModel(nn.Module):
         score = F.logsigmoid(score)
         neg_emb_v = self.v_embeddings(neg_v)
         neg_score = torch.bmm(neg_emb_v, emb_u.unsqueeze(2)).squeeze()
-        neg_score = torch.sum(neg_score, dim=1)
         neg_score = F.logsigmoid(-1 * neg_score)
         return -1 * (torch.sum(score)+torch.sum(neg_score))
 
@@ -83,7 +82,7 @@ class SkipGramModel(nn.Module):
             None.
         """
         if use_cuda:
-            embedding = self.u_embeddings.weight.cup().data.numpy()
+            embedding = self.u_embeddings.weight.cpu().data.numpy()
         else:
             embedding = self.u_embeddings.weight.data.numpy()
         fout = open(file_name, 'w')
